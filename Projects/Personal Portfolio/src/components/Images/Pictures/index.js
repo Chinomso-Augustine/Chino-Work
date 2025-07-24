5bba1db24119790e24f504e6b5c77b5f59d3bb53
Chino <chinomsoaugustine073@gmail.com>
2025-07-23 20:05:36 -0700

    Some bugs to fix

---
+++ Projects/Personal Portfolio/src/components/Images/Pictures/index.js
@@ -0,0 +1,88 @@
+import GradPics1 from "./Grad1.JPG";
+import GradPics2 from "./Grad2.JPG";
+import GradPics3 from "./Grad3.JPG";
+import GradPics4 from "./Grad4.JPG"
+import GradPics5 from "./Grad5.JPG";
+import GradPics6 from "./Grad6.JPEG";
+import GradPics7 from "./Grad7.JPEG";
+import GradPics8 from "./Grad8.JPEG";
+import GradPics9 from "./Grad9.JPEG";
+import GradPics10 from "./Grad10.JPEG";
+
+import GradPics11 from "./Grad11.JPEG";
+import GradPics12 from "./Grad12.JPEG";
+import GradPics13 from "./Grad13.JPEG";
+import GradPics14 from "./Grad14.JPEG";
+import GradPics15 from "./Grad15.JPEG";
+import GradPics16 from "./Grad16.JPEG";
+import GradPics17 from "./Grad17.JPEG";
+import GradPics18 from "./Grad18.JPEG";
+import GradPics19 from "./Grad19.JPEG";
+import GradPics20 from "./Grad20.JPEG";
+
+import GradPics21 from "./Grad21.JPEG";
+import GradPics22 from "./Grad22.JPEG";
+import GradPics23 from "./Grad23.JPEG";
+import GradPics24 from "./Grad24.JPEG";
+import GradPics25 from "./Grad25.JPEG";
+import GradPics26 from "./Grad26.JPEG";
+import GradPics27 from "./Grad27.JPEG";
+
+import Bd1 from "./Birthday1.JPEG";
+import Bd2 from "./Birthday2.JPEG";
+import Bd3 from "./Birthday3.JPEG";
+
+import CS1 from "./ColorStack1.JPEG"
+import CS2 from "./ColorStack2.JPEG"
+import CS3 from "./ColorStack3.JPEG"
+import CS4 from "./CS4.JPEG"
+import CS5 from "./CS5.JPEG"
+
+import Soccer1 from "./Soccer1.JPG"
+import Soccer2 from "./Soccer2.JPG"
+
+
+export {
+    GradPics1,
+    GradPics2,
+    GradPics3,
+    GradPics4,
+    GradPics5,
+    GradPics6,
+    GradPics7,
+    GradPics8,
+    GradPics9,
+    GradPics10,
+    GradPics11,
+    GradPics12,
+    GradPics13,
+    GradPics14,
+    GradPics15,
+    GradPics16,
+    GradPics17,
+    GradPics18,
+    GradPics19,
+    GradPics20,
+    GradPics21,
+    GradPics22,
+    GradPics23,
+    GradPics24,
+    GradPics25,
+    GradPics26,
+    GradPics27,
+
+    Bd1,
+    Bd2,
+    Bd3,
+
+    CS1,
+    CS2,
+    CS3,
+    CS4,
+    CS5,
+    CS6,
+
+    Soccer1,
+    Soccer2
+
+}
